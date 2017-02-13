276534d56dba8eb97a36e5bef9fc1d9e6dc22929
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2017-02-13 17:20:57 +0100

    refactored. Added config struct

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //    let search = args.get(1);
-    //    let filename = args.get(2);
-    let search = &args[1];
-    let filename = &args[2];
 
-    println!("Searching for {} in file {}.", search.trim(), filename.trim());
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
 
-    let mut file = File::open(filename).expect("File not found!");
+    println!("Searching for {} in file {}.", config.search, config.filename);
+
+    let mut file = File::open(config.filename).expect("File not found!");
     let mut content = String::new();
     file.read_to_string(&mut content).expect("Something went wrong reading the file!");
     println!("With text:\n{}", content);
+}
 
+struct Config {
+    search: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
+        let search = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {
+            search: search,
+            filename: filename,
+        })
+    }
 }
